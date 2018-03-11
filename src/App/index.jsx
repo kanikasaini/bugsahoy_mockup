eeba7ab70fb3ccfae103a00f45a46eb2c5c2de9b
Kanika Saini <kanika16047@iiitd.ac.in>
2018-03-11 14:59:08 +0530

    updated

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,11 +1,7 @@
 import { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { object } from 'prop-types';
-import { Helmet, link } from 'react-helmet';
-import { Grid } from 'react-bootstrap';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { loadable } from '../utils';
 import './styles.css';
-import iconUrl from '../taskcluster.png';
 
 const Home = loadable(() =>
   import(/* webpackChunkName: 'Home' */ '../views/Home')
--- src/components/Guide/index.jsx
+++ src/components/Guide/index.jsx
@@ -1,23 +1,17 @@
 import { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
-import { Col, Row, Grid, ListGroupItem, ListGroup } from 'react-bootstrap';
-import Icon from 'react-fontawesome';
-import chunk from 'lodash.chunk';
-import Markdown from '../../components/Markdown';
-import HelmetTitle from '../../components/HelmetTitle';
-import links from '../../links';
+import { ListGroupItem, ListGroup } from 'react-bootstrap';
 
 export default class Guide extends PureComponent {
   render() {
     return (
-    <div>
-    <h4> Contribution Guides </h4>
-     <ListGroup>
-    <ListGroupItem href="#link1">Taskcluster</ListGroupItem>
-    <ListGroupItem href="#link2">Telemetry</ListGroupItem>
-    <ListGroupItem href="#link2">SeaMonkey</ListGroupItem>
-  </ListGroup>
-  </div>
+      <div>
+        <h4> Contribution Guides </h4>
+        <ListGroup>
+          <ListGroupItem href="#link1">Taskcluster</ListGroupItem>
+          <ListGroupItem href="#link2">Telemetry</ListGroupItem>
+          <ListGroupItem href="#link2">SeaMonkey</ListGroupItem>
+        </ListGroup>
+      </div>
     );
   }
 }
